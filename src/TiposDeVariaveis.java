24240db321c0c4746add47e46681bedb43af6b1b
thamiris_arruda <thamydyanara@gmail.com>
2023-01-03 17:53:07 -0300

    add exercicios

---
+++ src/TiposDeVariaveis.java
@@ -0,0 +1,22 @@
+public class TiposVariaveis {
+
+public static void main(String[] args) throws Exception {
+    
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+    numero = 10;
+    System.out.println(numero);
+    
+  }  
+}
+
+//byte idade = 123;
+//short ano = 2023;
+//int cep = 5506390; //Se começar com zero, talvez seja string
+//long cpf = 78945612300L; //Se começar com zero, talvez seja string
+//float pi= 3.14F;
+//double salario = 5000.50;
+//final double VALOR_DE_PI = 3.14;
+//FINAL é o resultado final para não ter alterações
---
+++ src/logicos.java
@@ -0,0 +1,17 @@
+public class logicos {
+    public static void main (String [] args) {
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+        
+        System.out.println("fim");
+    }
+}
